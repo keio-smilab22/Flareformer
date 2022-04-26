ed1cde0871b1a59c78d98509a4ba39a25372c35d
Katsuyuki0007 <katsukuyo@gmail.com>
2022-04-26 19:43:26 +0900

    update eval_sparse

--- eval_sparse.py
+++ eval_sparse.py
@@ -4,12 +4,13 @@ import argparse
 import json
 # from mae.prod.datasets import *
 from src.Dataloader import TrainDataloader256, TrainDatasetSparse
+from tqdm import tqdm
 
 
 parser = argparse.ArgumentParser('MAE pre-training', add_help=False)
 parser.add_argument('--input_size', default=256, type=int)
 parser.add_argument('--checkpoint', default=10, type=int)
-parser.add_argument('--baseline', default="linear")
+parser.add_argument('--baseline', default="attn")
 parser.add_argument('--grid_size', default=16, type=int)
 args = parser.parse_args()
 
@@ -18,29 +19,42 @@ params = json.loads(open("params/params_2014.json").read())
 # img, _ = dl[0]
 # img = img.transpose((1, 2, 0))
 # dl = TrainDataloader256(split="train", params=params["dataset"],has_window=False)
-dl = TrainDatasetSparse(split="test", params=params["dataset"],has_window=False, grid_size=args.grid_size)
+dl = TrainDatasetSparse(split="test", params=params["dataset"],has_window=False, grid_size=args.grid_size, keep_ratio=0.1)
 
 mean, std = dl.calc_mean()
 dl.set_mean(*dl.calc_mean())
 
-img, _, _ = dl[-2]
+
+img, _, _ = dl[2]
 print(img.shape)
 img = img.transpose(0,1).transpose(1,2)
-
-# dl = FashionMnistDataLoader()
-# img, _, _ = dl[0]
-# print(img.shape)
-# img = img.transpose(0,1).transpose(1,2)
-# print(img.shape)
-
-# assert img.shape == (224, 224, 3)
 
 # normalize by ImageNet mean and std
 # img = img - imagenet_mean
 # img = img / imagenet_std
 
-plt.rcParams['figure.figsize'] = [5, 5]
-show_image(img.clone().detach())
+# plt.rcParams['figure.figsize'] = [24, 24]
+# for idx, data in tqdm(enumerate(dl), total=len(dl)):
+#     # if len(dl) - idx < 100:
+        
+#     #     img, std, _ = data
+#     #     img = img.transpose(0,1).transpose(1,2)
+#     #     # plt.subplot(int(len(dl)**0.5 + 1), int(len(dl)**0.5 + 1), idx+1)
+#     #     plt.subplot(10, 10, len(dl)-idx+1)
+#     #     show_image(img.clone().detach(), title=f"{idx}_{std:.4f}")
+#     if idx < 100:
+        
+#         img, std, _ = data
+#         img = img.transpose(0,1).transpose(1,2)
+#         # plt.subplot(int(len(dl)**0.5 + 1), int(len(dl)**0.5 + 1), idx+1)
+#         plt.subplot(10, 10, idx+1)
+#         show_image(img.clone().detach(), title=f"{idx}_{std:.4f}")
+#     else:
+#         break
+    
+# plt.show()
+
+
 
 
 # This is an MAE model trained with pixels as targets for visualization
@@ -49,7 +63,7 @@ show_image(img.clone().detach())
 # download checkpoint if not exist
 # !wget -nc https://dl.fbaipublicfiles.com/mae/visualize/mae_visualize_vit_large.pth
  
-chkpt_dir = f'/home/katsuyuki/temp/flare_transformer/output_dir/attn/checkpoint-50-sparse32_m0.pth'
+chkpt_dir = f'/home/katsuyuki/temp/flare_transformer/output_dir/attn/checkpoint-50-sparse128_m0.75_0.1-128.pth'
 # chkpt_dir = f'/home/initial/Dropbox/flare_transformer/output_dir/attn/checkpoint-5.pth'
 
 model_mae = prepare_model(chkpt_dir,img_size=args.input_size,baseline=args.baseline, embed_dim=64)
