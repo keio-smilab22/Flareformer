fe4e9696d96dfd787012ac773815ad20870c3f4c
YuigaWada <yuigawada@gmail.com>
2022-05-26 18:07:38 +0900

    [add] add a missing code

---
+++ utils/logs.py
@@ -0,0 +1,28 @@
+import wandb as wandb_runner
+from argparse import Namespace
+from dataclasses import dataclass
+from typing import Any, Dict, List
+
+
+@dataclass
+class Log:
+    stage: str
+    loss: float
+    score: Any
+
+
+class Logger:
+    def __init__(self, args: Namespace, wandb: bool) -> None:
+        if args.wandb:
+            wandb_runner.init(project=args.project_name, name=args.wandb_name)
+
+        self.wandb_enabled = wandb
+
+    def write(self, epoch: int, logs: List[Log]):
+        l: Dict[str, Any] = {"epoch": epoch}
+        for lg in logs:
+            l[f"{lg.stage}_loss"] = lg.loss
+            l.update(lg.score)
+
+        if self.wandb_enabled:
+            wandb_runner.log(l)
