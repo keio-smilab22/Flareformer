2e890d81515175478b10a433a5f9470f606caedb
Katsuyuki0007 <katsukuyo@gmail.com>
2022-05-27 21:53:21 +0900

    update train_reg.sh

--- train_reg.sh
+++ train_reg.sh
@@ -5,7 +5,14 @@
 
 # python main_informer.py --model FT --data Flare --des open_test --batch_size 64 --feat_path data_all_v2_2.csv
 
-python main_informer.py --model FT --data Flare --des closed_test_ffill --batch_size 64 --feat_path data_all_v2_ffill.csv --lradj none --train_epochs 20 --learning_rate 1e-5
+python main_informer.py --model FT --data Flare --des open_test_ffill --batch_size 64 --feat_path data_all_v2_ffill.csv --lradj none --train_epochs 20 --learning_rate 1e-5
+
+python main_informer.py --model FT --data Flare --des open_test_ffill --batch_size 64 --feat_path data_all_v2_bfill.csv --lradj none --train_epochs 20 --learning_rate 1e-5
+
+python main_informer.py --model FT --data Flare --des open_test --batch_size 64 --feat_path data_all_v2.csv --lradj none --train_epochs 20 --learning_rate 1e-5
+
+python main_informer.py --model FT --data Flare --des open_test_fillna0 --batch_size 64 --feat_path data_all_v2_fillna0.csv --lradj none --train_epochs 20 --learning_rate 1e-5
+
 
 # python main_informer.py --model FT --data Flare --des closed_test_interp_time --batch_size 64 --feat_path data_all_v2_interp_time.csv --learning_rate
 
