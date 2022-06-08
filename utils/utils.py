0be45f2af33864a0e9415f76ec1d9e286de083a2
YuigaWada <yuigawada@gmail.com>
2022-06-09 03:13:03 +0900

    [update] add t-SNE func

--- utils/utils.py
+++ utils/utils.py
@@ -1,9 +1,11 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import torch
 import math
 
 from argparse import Namespace
 from typing import Any, Dict
+from sklearn.manifold import TSNE
 
 
 def fix_seed(seed: int) -> None:
@@ -48,3 +50,23 @@ def adjust_learning_rate(optimizer, current_epoch, epochs, lr, args):  # optimiz
             param_group["lr"] = lr
 
     return lr
+
+
+def visualize_3D_tSNE(embedded: torch.Tensor, labels: torch.Tensor):
+    """
+    visualize 3d embedded vector using t-SNE.
+    """
+    if len(labels.shape) != 1:  # if labels are one-hot
+        labels = labels.argmax(axis=1)
+
+    tsne = TSNE(n_components=3, init='pca', random_state=0, perplexity=30, n_iter=1000)
+    reduced = tsne.fit_transform(embedded.cpu().numpy())
+
+    labels_np = labels.cpu().numpy()
+    N = np.max(labels_np) + 1
+    fig = plt.figure(figsize=(10, 10)).gca(projection='3d')
+    for i in range(N):
+        target = reduced[labels_np == i]
+        fig.scatter(target[:, 0], target[:, 1], target[:, 2], label=str(i), alpha=0.5)
+    fig.legend(bbox_to_anchor=(1.02, 0.7), loc='upper left')
+    plt.show()
