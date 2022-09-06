05b1da5de4e81025b9dd150aca9529aaca72fe5c
kosec-katayama <105256661+kosec-katayama@users.noreply.github.com>
2022-09-06 20:38:59 +0900

    [fix] improve error handling
    * [change]サーバー通信時にエラーハンドリングを追記
    
    * [change]json読み込み時のバグ修正
    
    * Gitコミッターを修正
    
    * [change]jsonの日付判定部分を修正
    
    * [change]break記入漏れ修正
    
    Co-authored-by: Natsuma Matsunaga <matsunaga.natsuma.git@sec.co.jp>

--- utils/server.py
+++ utils/server.py
@@ -92,23 +92,25 @@ class CallbackServer:
             jsonl_database_path = "data/ft_database_all17.jsonl"
             query = f"{date.year}-{date.month}-{date.day}-{date.hour}"
             targets = []
+            prob = []
+            status = "failed"
             with open(jsonl_database_path, "r") as f:
+                query_date = datetime.datetime.strptime(query, '%Y-%m-%d-%H').strftime('%Y-%m-%d-%H')
                 for line in f.readlines():
                     data = json.loads(line)
                     targets.append(data)
                     if len(targets) > 4:
                         targets.pop(0)
-                    query_date = datetime.datetime.strptime(query, '%Y-%m-%d-%H').strftime('%Y-%m-%d-%H')
                     target_date = datetime.datetime.strptime(data["time"], '%d-%b-%Y %H').strftime('%Y-%m-%d-%H')
                     if query_date == target_date:
+                        imgs = torch.cat([CallbackServer.get_tensor_image_from_path(t["magnetogram"]) for t in targets])
+                        phys = np.array([list(map(float, t["feature"].split(","))) for t in targets])[:, :90]
+                        print(imgs.shape)
+                        prob = callback(imgs, phys).tolist()
+                        status = "success"
                         break
 
-            imgs = torch.cat([CallbackServer.get_tensor_image_from_path(t["magnetogram"]) for t in targets])
-            phys = np.array([list(map(float, t["feature"].split(","))) for t in targets])[:, :90]
-
-            print(imgs.shape)
-            prob = callback(imgs, phys).tolist()
-            return JSONResponse(content={"probability": {"OCMX"[i]: prob[i] for i in range(len(prob))}})
+            return JSONResponse(content={"probability": {"OCMX"[i]: prob[i] for i in range(len(prob))}, "oneshot_status": status})
 
         @fapi.post(
             "/images/path",
@@ -124,23 +126,25 @@ class CallbackServer:
             locale.setlocale(locale.LC_TIME, 'en_US.UTF-8')
             jsonl_database_path = "data/ft_database_all17.jsonl"
             query = f"{date.year}-{date.month}-{date.day}-{date.hour}"
+            query_date = datetime.datetime.strptime(query, '%Y-%m-%d-%H')
+            finish_date = query_date + datetime.timedelta(hours=24)
             targets = []
-            cnt = None
             with open(jsonl_database_path, "r") as f:
                 for line in f.readlines():
                     data = json.loads(line)
-                    query_date = datetime.datetime.strptime(query, '%Y-%m-%d-%H').strftime('%Y-%m-%d-%H')
-                    target_date = datetime.datetime.strptime(data["time"], '%d-%b-%Y %H').strftime('%Y-%m-%d-%H')
-                    if query_date == target_date:
-                        cnt = 0
-                    if cnt is not None:
+                    target_date = datetime.datetime.strptime(data["time"], '%d-%b-%Y %H')
+                    if query_date < target_date and target_date <= finish_date:
                         targets.append(data)
-                        cnt += 1
-                        if cnt >= 24:
-                            break
+
+            if len(targets) == 0:
+                status = "failed"
+            elif len(targets) < 24:
+                status = "warning"
+            else :
+                status = "success"
 
             paths = [t["magnetogram"] for t in targets]
-            return JSONResponse(content={"images": paths})
+            return JSONResponse(content={"images": paths, "get_image_status": status})
 
         @fapi.get("/images/bin", response_class=FileResponse)
         def execute_oneshot(
