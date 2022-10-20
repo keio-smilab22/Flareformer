c2212540b1beaacf66735c9f1b65d18d434b52eb
Yoshiro KATAYAMA <yoshiro.katayama.git@sec.co.jp>
2022-10-20 19:22:04 +0900

    [change]単体テストを実行するshell追加

---
+++ tests/locust/script/test_oneshot_simple.sh
@@ -0,0 +1,24 @@
+# get date now
+test_date=`date "+%Y%m%d-%H%M%S"`
+
+# set path
+root_dir=$(cd `dirname $0`/../../../; pwd)
+config_dir=${root_dir}/tests/locust/config
+result_dir=$root_dir/tests/locust/result/${test_date}_oneshot_simple
+
+# make result dir
+mkdir -p $result_dir
+
+# start server 
+gnome-terminal -- bash -c "poetry run python src/flareformer/main.py --mode=server --params=config/params_2017.json > ${result_dir}/server.log ; exit"
+
+# wait server starting
+echo "** wait server starting (10 sec) **"
+sleep 10
+echo "** locust test start! **"
+
+# locust test run
+poetry run locust --config ${config_dir}/test_get_image_bin.conf --html ${result_dir}/locust.html
+
+# kill gnome-terminal
+pkill -f "poetry run python src/flareformer/main.py"
