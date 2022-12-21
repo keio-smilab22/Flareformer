3091402e32cc714139d13296b5712a8a42e719c5
YuigaWada <yuigawada@gmail.com>
2022-12-21 15:36:59 +0900

    [Rename] calc_params

--- utils/calc_params.py
+++ utils/calc_params.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import json
+import datetime
 
 
 def calc_gmgs_score_matrix(path_sunpy_csv):
@@ -59,6 +61,74 @@ def calc_gmgs_score_matrix(path_sunpy_csv):
     return gmgs_matrix
 
 
+
+def calc_gmgs_score_matrix_from_jsonl(path_jsonl):
+    """Calculate the GMGS score matrix from a csv file.
+
+    Parameters
+    ----------
+    path_jsonl : str
+        Path to the jsonl file with the label data.
+
+    Returns
+    -------
+    list[list[float]]
+        GMGS score matrix. 
+    """
+    # open jsonl file
+    with open(path_jsonl, 'r') as f:
+        data = f.readlines()
+    data = [json.loads(line) for line in data]
+
+    # get labels(flag) to 
+
+    labels = []
+    for i in range(len(data)):
+        # from start to 31-Nov-2016 23
+        time = datetime.datetime.strptime(data[i]['time'], "%d-%b-%Y %H")
+        if time < datetime.datetime.strptime('01-Jan-2016 00', "%d-%b-%Y %H"):
+            labels.append(data[i]['flag'])
+    # label
+    # "1,0,0,0" -> 0
+    # "0,1,0,0" -> 1
+    # "0,0,1,0" -> 2
+    # "0,0,0,1" -> 3
+    cnt = {0: 0, 1: 0, 2: 0, 3: 0}
+    for i in range(len(labels)):
+        if labels[i] == '1,0,0,0':
+            labels[i] = 0
+            cnt[0] += 1
+        elif labels[i] == '0,1,0,0':
+            labels[i] = 1
+            cnt[1] += 1
+        elif labels[i] == '0,0,1,0':
+            labels[i] = 2
+            cnt[2] += 1
+        elif labels[i] == '0,0,0,1':
+            labels[i] = 3
+            cnt[3] += 1
+        else:
+            print('ERROR')
+            return 0
+    p = []
+    for i in range(4):
+        p.append(cnt[i] / len(labels))
+    print(f'p = {p}')
+    print(f"p[0]+p[1]= {p[0]+p[1]}")
+    print(f"p[2]+p[3]= {p[2]+p[3]}")
+
+    gmgs_matrix = [[0 for i in range(4)] for j in range(4)]
+    for i in range(1, 5):
+        for j in range(i, 5):
+            if i == j:
+                gmgs_matrix[i - 1][i - 1] = s_ii(p, 4, i)
+            else:
+                gmgs_matrix[i - 1][j - 1] = gmgs_matrix[j - 1][i - 1] = s_ij(p, 4, i, j)
+    return gmgs_matrix
+
+
+
+
 def s_ii(p, N, i):
     sum_ak_inverse = 0
     sum_ak = 0
@@ -88,10 +158,16 @@ def s_ij(p, N, i, j):
 
 
 def a(p, i):
-    return p[i - 1] / (1 - p[i - 1])
+    sum_pk = 0
+    for x in range(i):
+        sum_pk += p[x]
+    return (1-sum_pk) / sum_pk
 
 
 if __name__ == '__main__':
     path_sunpy_csv = 'data/noaa/magnetogram_logxmax1h_all_years.csv'
-    gmgs_matrix = calc_gmgs_score_matrix(path_sunpy_csv)
+    path_jsonl = 'data/ft_database_all17.jsonl'
+    # gmgs_matrix = calc_gmgs_score_matrix(path_sunpy_csv)
+    # print(gmgs_matrix)
+    gmgs_matrix = calc_gmgs_score_matrix_from_jsonl(path_jsonl)
     print(gmgs_matrix)
